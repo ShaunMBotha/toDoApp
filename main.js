e4ed58811f7556cb93a2742f6664e6519b711705
ShaunMBotha <shaunmichaelbotha@gmail.com>
2022-03-24 19:50:07 +0200

    Edit function completed
    With help from Jaren with troubleshooting we figured out the bug with edit task.

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-// Add task to local storage
+//Add task
+
 let addBtn = document.getElementById("newSubmit");
-addBtn.addEventListener("click", function (e) {
+addBtn.addEventListener("click", (e) => {
 
     let addTask = document.getElementById("newTask");
     let addDate = document.getElementById("newDate");
-
-    
 
     if (addTask.value == "" || addDate.value == "") {
         return alert("Fill both the Task Description and the Due Date")
@@ -21,24 +20,29 @@ addBtn.addEventListener("click", function (e) {
         name: addTask.value,
         date: addDate.value
     }
+
     taskObj.push(myObj);
     localStorage.setItem("tasks", JSON.stringify(taskObj));
     addDate.value = "";
     addTask.value = "";
-    //   console.log(taskObj);
+    
     showTasks();
 });
 
-// Function to show elements from localStorage
+// Poppulate page with tasks
 function showTasks() {
+
     let tasksLi = localStorage.getItem("tasks");
+
     if (tasksLi == null) {
         taskObj = [];
     } else {
         taskObj = JSON.parse(tasksLi);
     }
+
     let html = "";
-    taskObj.forEach(function (element, index) {
+
+    taskObj.forEach((element, index) => {
         html += `
 
         <li>
@@ -48,16 +52,16 @@ function showTasks() {
                         <div>${index + 1}</div>
                     </div>
                     <div class="content">
-                        <input type="text" id="field" class="text" value="${element.name}" readonly>
+                        <input type="text" id="field${index}" class="text" value="${element.name}" readonly>
                     </div>
 
                     <div class="date">
-                        <input type="date" id="fieldD" class="date" readonly value="${element.date}">
+                        <input type="date" id="fieldD${index}" class="date" readonly value="${element.date}">
                     </div>
 
                     <div class="actions">
-                        <button id="${index}"onclick="editTask(this.id)" class="edit">Edit</button>
-                        <button id="${index}"onclick="deleteTask(this.id)" class="delete">Delete</button>
+                        <button id="${index}"onclick="editTask(${index})" class="edit">Edit</button>
+                        <button id="${index}"onclick="deleteTask(${index})" class="delete">Delete</button>
                     </div>
                 </div>
             
@@ -65,7 +69,9 @@ function showTasks() {
 
             `;
     });
+
     let taskElm = document.getElementById("tasks");
+
     if (taskObj.length != 0) {
         taskElm.innerHTML = html;
     } else {
@@ -73,10 +79,11 @@ function showTasks() {
     }
 }
 
-// Function to delete a task
+// Delete Function
 function deleteTask(index) {
 
     let confirmDel = confirm("Delete this task?");
+
     if (confirmDel == true) {
         let tasksLi = localStorage.getItem("tasks");
         if (tasksLi == null) {
@@ -89,52 +96,43 @@ function deleteTask(index) {
         localStorage.setItem("tasks", JSON.stringify(taskObj));
         showTasks();
     }
-
 }
 
-// Function to Edit the task
+// Edit Function
 function editTask(index) {
-    console.log(index);
+    
     let tasksLi = localStorage.getItem("tasks");
-    let editTask = document.getElementById("field");
-    let editDate = document.getElementById("fieldD");    
+    let editTask = document.getElementById("field".concat(index));
+    let editDate = document.getElementById("fieldD".concat(index));
     let editState = document.getElementById(index);
     
-    console.log(editState);
-
-    
+    let editTaskObj = JSON.parse(tasksLi);
 
     if (editState.innerText.toLowerCase() == "edit") {
-        console.log("made it 1");
         editState.innerText = "Save";
-        console.log(editState);
         editTask.removeAttribute("readonly");
         editTask.className = "text-edit";
         editTask.focus();
         editDate.removeAttribute("readonly");
     } else {
-        console.log("made it 2");
         editState.innerText = "Edit";
         editTask.setAttribute("readonly", "readonly");
         editTask.className = "text";
-        console.log(editTask.innerHTML);
         editDate.setAttribute("readonly", "readonly");
-        editTask = document.getElementById("field");
-        editDate = document.getElementById("fieldD");
-
-        let editObj = {
+        editTask = document.getElementById("field".concat(index));
+        editDate = document.getElementById("fieldD".concat(index));
+       
+        editTaskObj [index] = {
             name: editTask.value,
             date: editDate.value
         }
-        taskObj.splice(index, 1);
-        taskObj.push(editObj);
-        localStorage.setItem("tasks", JSON.stringify(taskObj));
+        
+        localStorage.setItem("tasks", JSON.stringify(editTaskObj));
         showTasks();
 
 
     }
 
 }
-
 
 showTasks();
